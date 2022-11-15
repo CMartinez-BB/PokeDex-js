e5ac85f99c2d902e2b501e5ca37afb07dca982dd
CMartinez-BB <charli.rey.misterio@gmail.com>
2022-11-15 15:48:59 -0600

    Adding new function to display new elements of pokemon

--- js/main.js
+++ js/main.js
@@ -6,7 +6,17 @@ const type_pokemon = document.getElementById('type')
 const life_pokemon = document.getElementById('life')
 let search = document.getElementById('api-btn')
 
-
+window.
+onload
+= function() {
+  Particles.
+init
+({
+    selector: 
+'.background',
+    color: '#DC3545'
+  });
+};
 
 // create action for button api
 apiBtn.addEventListener ('click', search);
@@ -57,11 +67,16 @@ function generatePokemon(poke) {
     
     const name_poke = document.createElement('p')
     name_poke.classList.add('name')
-    name_poke.textContent = poke.name;
+    name_poke.textContent ="name"+ poke.name;
+
+    const ability_pokemon = document.createElement('p')
+    ability_pokemon.classList.add('Ability')
+    ability_pokemon.textContent = "Ability "+poke.abilities[0].ability.name;
 
     card.appendChild(spriteContainer)
     card.appendChild(number)
     card.appendChild(name_poke)
+    card.appendChild(ability_pokemon)
     
     // const type = poke.types[0].type.name;
     // const hp = poke.stats[0].base_stat
@@ -74,4 +89,4 @@ function generatePokemon(poke) {
 
 }
 
-showPokemons(9);
+showPokemons(6);
